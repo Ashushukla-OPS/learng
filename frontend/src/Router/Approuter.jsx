216778b047b7785665844f355ecc6c31d05e23bb
Ashutosh shukla <ashu.dhawari@gmail.com>
2026-05-18 16:29:40 +0530

    Fix Vercel import casing

--- frontend/src/Router/Approuter.jsx
+++ frontend/src/Router/Approuter.jsx
@@ -1,117 +1,113 @@
-import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "../App";
-import Register from  "../pages/auth/Register"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
+import Forgotpassword from "../pages/auth/Forgotpassword";
+import Resetpassword from "../pages/auth/Resetpassword";
+
 import Dashboard from "../pages/dashboard/Dashboard";
-import EditProfile from "../pages/profile/EditProfile";
-import ForgotPassword from "../pages/auth/ForgotPassword";
-import ResetPassword from "../pages/auth/ResetPassword";
-import AllJournals from "../pages/journals/AllJournals";
+
+import AllJournal from "../pages/journals/AllJournal";
 import AddJournal from "../pages/journals/AddJournal";
 import JournalDetails from "../pages/journals/JournalDetails";
 import EditJournal from "../pages/journals/EditJournal";
+
 import Profile from "../pages/profile/Profile";
+import EditProfile from "../pages/profile/EditProfile";
+
 import ProtectedRoute from "../components/ProtectedRoute";
 import PublicRoute from "../components/PublicRoute";
 function Approuter() {
-    const router = createBrowserRouter([
-      {
-        path: "/",
-        element: (
-          <PublicRoute>
-            <Register />
-          </PublicRoute>
-        ),
-      },
-      {
-        path: "/login",
-        element: (
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        ),
-      },
-      {
-        path: "/Dashboard",
-        element: (
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/forgot-password",
-        element: (
-          
-            <Forgotpassword />
-          
-        ),
-      },
-
-      {
-        path: "/reset-password/:token",
-        element: (
-          
-            <Resetpassword />
-          
-        ),
-      },
-
-      {
-        path: "/journals",
-
-        element: (
-          <ProtectedRoute>
-            <Alljournal />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: "/journals/add",
-        element: (
-          <ProtectedRoute>
-            <Addjournal />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: "/journals/:id",
-        element: (
-          <ProtectedRoute>
-            <JournalDetails />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: "/journals/edit/:id",
-        element: (
-          <ProtectedRoute>
-            <EditJournal />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: "/profile",
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        ),
-      },
-
-      {
-        path: "/profile/edit",
-        element: (
-          <ProtectedRoute>
-            <EditProfile />
-          </ProtectedRoute>
-        ),
-      },
-    ]);
+   const router = createBrowserRouter([
+     {
+       path: "/",
+       element: (
+         <PublicRoute>
+           <Register />
+         </PublicRoute>
+       ),
+     },
+     {
+       path: "/login",
+       element: (
+         <PublicRoute>
+           <Login />
+         </PublicRoute>
+       ),
+     },
+     {
+       path: "/forgot-password",
+       element: (
+         <PublicRoute>
+           <Forgotpassword />
+         </PublicRoute>
+       ),
+     },
+     {
+       path: "/reset-password/:token",
+       element: (
+         <PublicRoute>
+           <Resetpassword />
+         </PublicRoute>
+       ),
+     },
+     {
+       path: "/dashboard",
+       element: (
+         <ProtectedRoute>
+           <Dashboard />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/journals",
+       element: (
+         <ProtectedRoute>
+           <AllJournal />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/journals/add",
+       element: (
+         <ProtectedRoute>
+           <AddJournal />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/journals/:id",
+       element: (
+         <ProtectedRoute>
+           <JournalDetails />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/journals/edit/:id",
+       element: (
+         <ProtectedRoute>
+           <EditJournal />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/profile",
+       element: (
+         <ProtectedRoute>
+           <Profile />
+         </ProtectedRoute>
+       ),
+     },
+     {
+       path: "/profile/edit",
+       element: (
+         <ProtectedRoute>
+           <EditProfile />
+         </ProtectedRoute>
+       ),
+     },
+   ]);
   return (
     <RouterProvider  router={router}/>
   );
